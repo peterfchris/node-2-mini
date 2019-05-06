bb225ecec4d99207ba3eacb0739253be34bc24d7
Peter Forsgren Christensen <peter.f.chris@gmail.com>
2019-05-06 12:21:30 -0600

    completed endpoints

---
+++ server/controllers/books_controller.js
@@ -0,0 +1,36 @@
+let books = []
+let id = 0
+
+module.exports = {
+    read(req, res) {
+        res.send(books)
+    },
+    create(req, res) {
+        let { title, author } = req.body
+        books.push({
+            id, 
+            title,
+            author
+        })
+        id++
+        res.send(books)
+    },
+    update(req, res) {
+        let { id } = req.params
+        let { title, author } = req.body 
+        let index = books.findIndex( book => book.id === parseInt(id))
+        if(index !== -1) {
+            books[index] = {
+                id: books[index].id,
+                title: title ? title : books[index].title,
+                author: author ? author : bookx[index].author
+            }
+        }
+    },
+    delete(req, res) {
+        let { id } = req.params
+        let index = books.findIndex( book => book.id === parseInt(id))
+        if (index !== -1) books.splice(index, 1)
+        res.send(books)
+    }
+}
